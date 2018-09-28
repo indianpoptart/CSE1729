a0d4279a5e419efa08bda2f262ac0158eccd73fc
Nikhil Paranjape <admin@nikhilp.org>
2018-09-28 08:35:17 -0400

    n

--- 2018-09-27.rkt
+++ 2018-09-27.rkt
@@ -4,3 +4,18 @@
       (+ (f n) (sum f (- n 1)))))
 
 
+(define (f x)
+  (define (g y) (+ x y))
+  (define (h x) (+ x (g x)))
+  (h (+ x 10)))
+(f 100)
+
+(define (fact n)
+  (if (= n 0)
+      1
+      (* n (fact (- n 1)))))
+
+(define (fact-accumulate n a)
+  (if (= n 0) a
+      (fact-accumulate (- n 1)
+                       (* n a))))

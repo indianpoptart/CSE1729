3f1c6fafcd898b7a0ce040aa49992a449b4c6ae1
Nikhil Paranjape <admin@nikhilp.org>
2018-11-02 03:32:09 -0400

    4qwaef

--- cse-11-01-2018.rkt
+++ cse-11-01-2018.rkt
@@ -1,4 +1,11 @@
 (define (f x)
   (lambda (y)
-    (+ x y)))
+    (+ z (* x y))))
 (define y (f 3))
+
+(define (member? elt set)
+  (cond
+    ((null? set) #f)
+    ((eq? elt (car set)) #t)
+    (else
+     (member? elt (cdr set)))))

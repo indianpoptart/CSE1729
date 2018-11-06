a775f632bcd989609b9b0e5e272cb2f2b044aec0
Nikhil Paranjape <admin@nikhilp.org>
2018-11-06 09:11:11 -0500

    ghtsegrawfs

--- paranjape-nikhil-CSE1729-problem-set5.rkt
+++ paranjape-nikhil-CSE1729-problem-set5.rkt
@@ -15,7 +15,14 @@
       (else
        #f))))
 
+(display "\n(is-prefix? '(1) '(1 2 3)) = ")
 (is-prefix? '(1) '(1 2 3))
+
+(display "\n(is-prefix? '(2) '(1 2 3)) = ")
+(is-prefix? '(2) '(1 2 3))
+
+(display "\n(is-prefix? '(7) '(7 6 5 4 3 2 1)) = ")
+(is-prefix? '(7) '(7 6 5 4 3 2 1))
 
 ;Problem 1b
 (display "\nProblem 1b\n")
@@ -29,7 +36,14 @@
        (cons (car L1)(longest-common-prefix (cdr L1)(cdr L2))))
       (else '()))))
 
+(display "\n(longest-common-prefix '(1 2) '(1 2 3)) = ")
 (longest-common-prefix '(1 2) '(1 2 3))
+
+(display "\n(longest-common-prefix '(1 2 3) '(1 2 3 4 5 6)) = ")
+(longest-common-prefix '(1 2 3) '(1 2 3 4 5 6))
+
+(display "\n(longest-common-prefix '(1 2) '(3 6 7 1 2 9 4)) = ")
+(longest-common-prefix '(1 2) '(3 6 7 1 2 9 4))
 
 ;Problem 2a
 (display "\nProblem 2a\n")
@@ -48,7 +62,16 @@
 (define (gen-consecutive f a b)
   (map f (list-gen a b)))
 
+(display "\n(gen-consecutive (lambda(x)(* x x)) 1 10)   = ")
 (gen-consecutive (lambda(x)(* x x)) 1 10)
+
+(display "\n(gen-consecutive (lambda(x)(* x x x)) 1 10) = ")
+(gen-consecutive (lambda(x)(* x x x)) 1 10)
+
+(display "\n(gen-consecutive (lambda(x)(* x x)) 10 20)  = ")
+(gen-consecutive (lambda(x)(* x x)) 10 20)
+
+
 
 ;Problem 2b
 (display "\nProblem 2b\n")
@@ -65,7 +88,14 @@
         acc
       (factorial (- n 1)  (* acc n )))))
 
+(display "\n(gen-sequence factorial 1 5 (lambda(x)(+ x 2)))  = ")
 (gen-sequence factorial 1 5 (lambda(x)(+ x 2)))
+
+(display "\n(gen-sequence factorial 1 10 (lambda(x)(+ x 4))) = ")
+(gen-sequence factorial 1 10 (lambda(x)(+ x 4)))
+
+(display "\n(gen-sequence factorial 1 15 (lambda(x)(+ x 6))) = ") 
+(gen-sequence factorial 1 15 (lambda(x)(+ x 6)))
 
 ;Problem 3
 (display "\nProblem 3\n")
@@ -82,6 +112,12 @@
 (display "\n(filter even? '(1 2 3 4 5 6 7)) = ")
 (filter even? '(1 2 3 4 5 6 7))
 
+(display "\n(filter odd? '(1 2 3 4 5 6 7))  = ") 
+(filter odd? '(1 2 3 4 5 6 7))
+
+(display "\n(filter odd? '(1 2 3 4 5 6 7 8 9 10 11 12)) = ")
+(filter odd? '(1 2 3 4 5 6 7 8 9 10 11 12))
+
 ;Problem 4a
 (display "\nProblem 4a\n")
 
@@ -91,7 +127,14 @@
         (car L1)
         (value-at-position (cdr L1) (- k 1)))))
 
+(display "\n(value-at-position '(1 2 3 4 5 6) 3)   = ")
 (value-at-position '(1 2 3 4 5 6) 3)
+
+(display "\n(value-at-position '(7 6 5 4 3 2 1) 3) = ")
+(value-at-position '(7 6 5 4 3 2 1) 3)
+
+(display "\n(value-at-position '(7 6 8 3 2 9 22 12 35 12) 7) = ")
+(value-at-position '(7 6 8 3 2 9 22 12 35 12) 7)
 
 ;Problem 4b
 (display "\nProblem 4b\n")
@@ -101,6 +144,11 @@
     (cond ((< n (* d d)) #t)
           ((zero? (modulo n d)) #f)
           (else (loop (+ d 1))))))
+
+(define (range a b)
+  (if (> a b)
+      '()
+      (cons a (range (+ 1 a) b))))
 
 (define consecutive-ints
   (lambda (n m)
@@ -112,11 +160,21 @@
   (lambda (a b n)
     (value-at-position (filter is-prime? (consecutive-ints a b)) n)))
 
+(display "\n(nth-prime-between 1 10 2)    = ")
+(nth-prime-between 1 10 2)
+
+(display "\n(nth-prime-between 1 100 6)   = ")
+(nth-prime-between 1 100 6)
+
+(display "\n(nth-prime-between 1 1000 16) = ")
+(nth-prime-between 1 1000 16)
+
 ;Problem 5a
 (display "\nProblem 5a\n")
 
 (define square
   (lambda (x) (* x x)))
+
 (define (make-complex a b)
   (cons a b))
 
@@ -126,16 +184,68 @@
 (define (imag x)
   (cdr x))
 
+(define sgn
+      (lambda (v)
+        (cond ((< v 0) -1)
+              ((= v 0) 0)
+              (else 1))))
+
 (define complex-sqrt
   (lambda (x)
     (make-complex
      (sqrt (/ (+ (real x) (sqrt (+ (square (real x)) (square (imag x))))) 2))
-     (sqrt (/ (+ (- (real x)) (sqrt (+ (square (real x)) (square (imag x))))) 2)))))
-    
+     (* (sgn (imag x))(sqrt (/ (+ (- (real x)) (sqrt (+ (square (real x)) (square (imag x))))) 2))))))
+
+(display "
 (define blop (make-complex 5 6))
+
+(define blip (make-complex 2 4))
+
+(define bleep (make-complex 5 0))
+\n")
+
+(define blop (make-complex 5 6))
+
+(define blip (make-complex 2 4))
 
 (define bleep (make-complex 5 0))
 
+(display "\n(complex-sqrt blop)  = ")
 (complex-sqrt blop)
 
+(display "\n(complex-sqrt blip)  = ")
+(complex-sqrt blip)
+
+(display "\n(complex-sqrt bleep) = ")
 (complex-sqrt bleep)
+
+;Problem 5b
+(display "\nProblem 5b\n")
+
+(display "\n(complex-sqrt (make-complex -2 0)) = ")
+(complex-sqrt (make-complex -2 0))
+
+(display "\nWhen B = 0 there is a fault in complex-sqrt, the imaginary portion returns 0")
+
+;Problem 5c
+(display "\nProblem 5c\n")
+
+(define better-complex-sqrt
+  (lambda (x)
+    (define sgn
+      (lambda (v)
+        (cond ((< v 0) -1)
+              (else 1))))
+    (let ((root (sqrt (+ (square (real x)) (square (imag x))))))
+      (make-complex (sqrt (/ (+ (real x) root) 2))
+                    (* (sgn (imag x))
+                       (sqrt (/ (- root (real x)) 2)))))))
+
+(display "\n(better-complex-sqrt blop)  = ")
+(complex-sqrt blop)
+
+(display "\n(better-complex-sqrt blip)  = ")
+(complex-sqrt blip)
+
+(display "\n(better-complex-sqrt bleep) = ")
+(better-complex-sqrt bleep)
